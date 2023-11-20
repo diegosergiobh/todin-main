304a8c9104760a57fb4c128d93f615931e06a309
matheuskiyoshi <matheuskiyoshi@gmail.com>
2023-11-20 14:56:21 -0300

    feat(routing) : adicionando novas rotas ao app-routing.module

--- todin-main/src/app/app.module.ts
+++ todin-main/src/app/app.module.ts
@@ -1,19 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LoginComponent } from './core/auth/components/login/login.component';
-import { TasksListComponent } from './modules/tasks/components/tasks-list/tasks-list.component';
-import { CreateTaskComponent } from './modules/tasks/components/create-task/create-task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    TasksListComponent,
-    CreateTaskComponent
   ],
   imports: [
     BrowserModule,
