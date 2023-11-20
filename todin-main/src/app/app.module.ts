baecb578e8d58f631aa6827914814d5b8ba2f482
matheuskiyoshi <matheuskiyoshi@gmail.com>
2023-11-20 13:35:13 -0300

    feat(create-task): criação do componente para criar tasks

--- todin-main/src/app/app.module.ts
+++ todin-main/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LoginComponent } from './core/auth/components/login/login.component';
 import { TasksListComponent } from './modules/tasks/components/tasks-list/tasks-list.component';
+import { CreateTaskComponent } from './modules/tasks/components/create-task/create-task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    TasksListComponent
+    TasksListComponent,
+    CreateTaskComponent
   ],
   imports: [
     BrowserModule,
